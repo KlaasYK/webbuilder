b6958e63d837669becf93d4b26dffffda05ab2a9
KlaasYK <klaasyk@users.noreply.github.com>
2015-09-03 12:21:55 +0200

    Initial project layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mavenproject
-Base Maven project in java with logging (log4j) and executable jar packaging
+Base Maven project in java with logging (log4j via SLF4J) and executable jar packaging
+
+Logging is both done to STDOUT and file (/logs/test.log). For configuration see /src/main/resources/log4j.properties
---
+++ src/main/java/org/examle/appname/App.java
@@ -0,0 +1,18 @@
+package org.examle.appname;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	private static final String APPNAME = "Appname";
+	private static final String VERSION = "v0.0.1";
+	
+    public static void main( String[] args )
+    {
+    	Logger l = LoggerFactory.getLogger(App.class);
+    	l.info("{} {}", APPNAME, VERSION);
+        System.out.println( "Hello World!" );
+    }
+
+}
